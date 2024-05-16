4b5ffd5fb10f607c4b545ee3e55a7ef2fe92bc0c
davidhermosilla <davidhermosillagarzon@gmail.com>
2024-05-16 17:06:37 +0200

    Added CorsConfig

---
+++ src/main/java/com/club/padel/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.club.padel;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/club-padel/**") // Especifica el path de tu API
+                .allowedOrigins("*") // Permite el acceso desde este origen
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos HTTP permitidos
+                .allowedHeaders("*"); // Cabeceras permitidas
+    }
+}
